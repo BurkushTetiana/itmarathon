b1cbcd901c74f6f577ec8cb19faf43e2c9ff113a
Тетяна Буркуш <Tetiana.Burkush@cs.khpi.edu.ua>
2025-11-08 01:58:33 +0200

    Add S3 bucket2

--- terraform/backend.tf
+++ terraform/backend.tf
@@ -1,6 +1,6 @@
 terraform {
   backend "s3" {
-    bucket       = "terraform-tfstate-firstfxbaget"
+    bucket       = "firstfxbaget"
     key          = "terraform.tfstate"
     region       = "eu-central-1"
     use_lockfile = true
