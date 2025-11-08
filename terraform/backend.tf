7f29a63483db1888562b3b3076a23c67b4f942f1
BurkushTetiana <tanyaburkush@gmail.com>
2025-11-08 02:08:32 +0200

    Change S3 baget right

--- terraform/backend.tf
+++ terraform/backend.tf
@@ -1,6 +1,6 @@
 terraform {
   backend "s3" {
-    bucket       = "jkbskefbjek"
+    bucket       = "firstfxbaget"
     key          = "terraform.tfstate"
     region       = "eu-central-1"
     use_lockfile = true
