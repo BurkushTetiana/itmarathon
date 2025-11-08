c41c9b91b4666cff1258b43f32de96c06a5ae39a
BurkushTetiana <tanyaburkush@gmail.com>
2025-11-08 02:24:52 +0200

    try use_lockfile = true
    Removed the use_lockfile option from the S3 backend configuration.

--- terraform/backend.tf
+++ terraform/backend.tf
@@ -3,7 +3,7 @@ terraform {
     bucket       = "firstfxbaget"
     key          = "terraform.tfstate"
     region       = "eu-central-1"
-    use_lockfile = true
+   
     encrypt      = true
   }
 }
